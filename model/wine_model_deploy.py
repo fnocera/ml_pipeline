74b80f506bab88fe1b2ef9e3a830b2371da315cf
Federica Nocera <federica.nocera@gmail.com>
2019-05-08 15:38:03 -0400

    Train and deploy model with mlflow and aml.

--- model/wine_model_deploy.py
+++ model/wine_model_deploy.py
@@ -7,6 +7,7 @@ import mlflow.azureml
 
 from azureml.core import Workspace
 from azureml.core.webservice import AciWebservice, Webservice
+from azureml.core.authentication import InteractiveLoginAuthentication
 
 ENV = Env()
 ENV.read_env()
@@ -18,16 +19,40 @@ data_path = pathlib.Path().absolute() / "data/wine-quality.csv"
 workspace_name = ENV("AML_WORKSPACE")
 subscription_id = ENV("AZURE_SUBSCRIPTION_ID")
 resource_group = ENV("AZURE_RESOURCE_GROUP")
+tenant_id = ENV("AZURE_TENANT_ID")
 location = ENV("AZURE_RESOURCE_LOCATION")
-azure_workspace = Workspace.create(name=workspace_name,
-                                   subscription_id=subscription_id,
-                                   resource_group=resource_group,
-                                   location=location,
-                                   create_resource_group=True,
-                                   exist_okay=True)
+server_root = ENV("MLFLOW_SERVER_DEFAULT_ARTIFACT_ROOT")
+storage_key = ENV("AZURE_STORAGE_ACCESS_KEY")
 
-# Build an Azure ML container image for deployment
-azure_image, azure_model = mlflow.azureml.build_image(model_path="<path-to-model>",
+# azure_workspace = Workspace.create(name=workspace_name,
+#                                    subscription_id=subscription_id,
+#                                    resource_group=resource_group,
+#                                    location=location,
+#                                    create_resource_group=False,  # Change this to True if you want the rg created.
+#                                    exist_okay=True,
+#                                    show_output=True)
+
+interactive_auth = InteractiveLoginAuthentication(tenant_id=tenant_id)
+
+azure_workspace = Workspace.get(name=workspace_name,
+                                auth=interactive_auth,
+                                subscription_id=subscription_id,
+                                resource_group=resource_group
+                                )
+
+# For running locally saved model give local path
+# model_path = "model/mlruns/0/be672f421b2044c3b9fd605e67df8aa0/artifacts/model/"
+
+experiment_id = mlflow.set_experiment("wine_exp")
+
+# For running blob specific model
+model_path = "model_test_wine"
+run_id = "07a2f2024b114170b3e72a9839b4e145"
+
+mlflow.tracking.set_tracking_uri("http://0.0.0.0:5000")
+
+azure_image, azure_model = mlflow.azureml.build_image(model_path=model_path,
+                                                      run_id=run_id,
                                                       workspace=azure_workspace,
                                                       description="Wine regression model 1",
                                                       synchronous=True)
@@ -37,7 +62,7 @@ print("Access the following URI for build logs: {}".format(azure_image.image_bui
 # Deploy the container image to ACI
 webservice_deployment_config = AciWebservice.deploy_configuration()
 webservice = Webservice.deploy_from_image(
-                    image=azure_image, workspace=azure_workspace, name="<deployment-name>")
+                    image=azure_image, workspace=azure_workspace, name="winetestdeploymentremote")
 webservice.wait_for_deployment()
 
 # After the image deployment completes, requests can be posted via HTTP to the new ACI
@@ -50,10 +75,10 @@ import requests
 import json
 
 # `sample_input` is a JSON-serialized pandas DataFrame with the `split` orientation
-sample_input = pd.read_csv(data_path).head().drop(["quality"], axis=1).to_json(orient='split')
+sample_input = pd.read_csv(data_path).head(2).drop(["quality"], axis=1).to_json(orient='split')
 
 response = requests.post(
-              url=webservice.scoring_uri, data=json.dumps(sample_input),
+              url=webservice.scoring_uri, data=sample_input,
               headers={"Content-type": "application/json"})
 response_json = json.loads(response.text)
 print(response_json)
--- model/wine_model_train.py
+++ model/wine_model_train.py
@@ -7,6 +7,7 @@ import pandas as pd
 import pathlib
 import numpy as np
 import sys
+from environs import Env
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
@@ -16,6 +17,14 @@ import mlflow
 import mlflow.sklearn
 
 np.random.seed(20)
+
+ENV = Env()
+ENV.read_env()
+
+# To set up the server to store on blob I had to run: Make sure you pip install azure-storage
+# mlflow server --file-store /tmp/mlflow --default-artifact-root "$MLFLOW_SERVER_DEFAULT_ARTIFACT_ROOT" --host 0.0.0.0
+# Then run: python model/wine_model_train.py "model_test_wine" 0.7 0.2
+
 
 
 def eval_metrics(actual, pred):
@@ -39,21 +48,27 @@ def train(model_name, alpha, l1_ratio):
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    # Useful for multiple runs (only doing one run in this sample notebook)  
-    with mlflow.start_run():
+    # This should reflect wherever you have the server running and attach whatever storage you want.
+    mlflow.tracking.set_tracking_uri("http://0.0.0.0:5000")
+
+    # Set experiment which will create experiment if it does not already exist
+    experiment_id = mlflow.set_experiment("wine_exp")
+
+    # Useful for multiple runs (only doing one run in this sample notebook)
+    with mlflow.start_run(experiment_id=experiment_id):
         # Execute ElasticNet
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
         # Evaluate Metrics
         predicted_qualities = lr.predict(test_x)
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        rmse, mae, r2 = eval_metrics(test_y, predicted_qualities)
 
         # Print out metrics
         print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
+        print("RMSE: %s" % rmse)
+        print("MAE: %s" % mae)
+        print("R2: %s" % r2)
 
         # Log parameter, metrics, and model to MLflow
         mlflow.log_param("alpha", alpha)
@@ -63,6 +78,17 @@ def train(model_name, alpha, l1_ratio):
         mlflow.log_metric("mae", mae)
 
         mlflow.sklearn.log_model(lr, model_name)
+
+# Currently just storing locally but ideally want to do it in Azure blob:
+# wasbs://<container>@<storage-account>.blob.core.windows.net/<path>
+# set AZURE_STORAGE_CONNECTION_STRING
+
+"""
+mlflow server 
+--backend-store-uri wasbs://mlflowserver@fenoceraamlwor0346003249.blob.core.windows.net/runs
+--default-artifact-root wasbs://mlflowserver@fenoceraamlwor0346003249.blob.core.windows.net/artifacts
+
+"""
 
 
 if __name__ == "__main__":
